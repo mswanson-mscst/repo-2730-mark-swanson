7701634116c677cdce3be3b0a051f3992eaa8101
Mark Swanson <mswanson@southeastmn.edu>
2016-09-16 18:12:09 -0500

    Added Q1 Section 2 review

---
+++ mswansonQ1Review2/Form1.vb
@@ -0,0 +1,24 @@
+﻿' COMC2730 Quiz 1 Part 2 Review
+' Mark Swanson
+
+Option Strict On
+
+Public Class Form1
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        txtName.Text = "MsgA"
+        'Label6.Text = "MsgB"
+        'lblNet.Text = Convert.ToString(Convert.ToInt32(txtHours.Text) + 10)
+        'lblNet.Text = Convert.ToString(Convert.ToInt32(txtHours.Text) + PictureBox1.Top)
+        'PictureBox1.Top = 100
+        'PictureBox1.Top = PictureBox1.Top + Convert.ToInt32(txtHours.Text)
+        'txtName.Text = "MsgC" + "MsgD"
+        'txtName.Text = "MsgC" & "MsgD"
+        'txtName.Text = "Gross pay: " _
+        '    + Convert.ToString(Convert.ToInt32(txtHours.Text) * Convert.ToDouble(txtPayRate.Text))
+        'txtName.Text = "Gross pay: " + Convert.ToString(Convert.ToDecimal(txtHours.Text) * Convert.ToDecimal(txtPayRate.Text))
+        'lblNet.Text = Format(1000, "N2")
+        'lblNet.Text = 1000.ToString("N2")
+        'lblNet.Text = (Convert.ToDouble(txtPayRate.Text) * 1000).ToString("N2")
+        'txtName.Text = "MsgD: " + (Convert.ToDouble(txtPayRate.Text) * 1000).ToString("N2")
+    End Sub
+End Class
---
+++ mswansonQ1Review2/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("mswansonQ1Review2")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("mswansonQ1Review2")>
+<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("13ff6d2b-1c21-4719-8c40-4c6f051dcce0")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
